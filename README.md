d767663b63ba0e7593ba0208eaa109d557d1125d
Giuseppe Guarino <gmguarino1@gmail.com>
2020-08-25 13:43:10 +0200

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# DockerCompose
+# WAQI Docker
+
+This environment runs two docker containers to produce an applet that retrieves the air quality index in the current location, using the World Air Quality Index (WAQI) API.
+
+## The Containers
+The `docker-compose.yml` script launches two containers:
+1. The first container is a `python:3.7-alpine` container that runs a flask app to comunicate with the WAQI server.
+2. The second container runs a PostgreSQL image (`postgres:latest`) to store retrieved data.
+
+## Setup
+To setup the WAQI API you need to first register on [Air Quality Open Data Platform] and retieve an API token. Secondly, edit line 18 in the app.py script to add you API token.
+```
+API_TOKEN = "INSERT YOUR API TOKEN HERE"
+```
+You are now good to go!
+
+[Air Quality Open Data Platform]:https://aqicn.org/data-platform/token/#/
+
+## Running
+Clone the repository locally and from the main folder run: </br>
+```
+docker-compose up -d
+```
+(`-d` runs in detached mode ie. background)</br></br>
+The app will be visible on `localhost:5000`.</br>
+To stop the containers, run:
+```
+docker-compose down
+```
+## The App
+As previously said the app will be visible on  `localhost:5000`, and will load the current air quality on access.
+
+A recent history can also be viewed at  `localhost:5000/history/`. As a default this will show only the last entry in the database. To specify the number of entries to be visualised use
+```
+localhost:5000/history/?limit=<limit>
+```
+Where `<limit>` is to be substituted by the number to required entries.
--- app.py
+++ app.py
@@ -15,8 +15,8 @@ POSTGRES_HOST = 'dbhost'
 POSTGRES_PORT = '5432'
 
 # Token per le chiamate alla API della world air quality index
+# API_TOKEN = "INSERT YOUR API TOKEN HERE"
 API_TOKEN = "701db77838d1e5156e628c19086cc420840f0066"
-
 VARIABLE_DICT = {'t' : 'Temperature',
                  'h' : 'Humidity',
                  'p' : 'Pressure',
@@ -38,7 +38,7 @@ def connect_to_db():
                                   port = POSTGRES_PORT,
                                   database = POSTGRES_DB)
             cursor = connection.cursor()
-            conn.set_session(autocommit=True)
+            connection.set_session(autocommit=True)
             
 
         except (Exception, psycopg2.Error) as exc:
@@ -81,6 +81,40 @@ def get_air_data():
     else:
         return "404 HTTP Error: Not Found!"
 
+def format_history(history):
+    data = dict()
+    labels = [
+        "Index",
+        "StationName",
+        "Temperature",
+        "Pressure",
+        "Humidity",
+        "PM10",
+        "PM25"
+    ]
+
+    for idx, val in enumerate(history):
+        data[labels[idx]] = val
+
+    formatted = f'''
+        <h3>Entry number: {data["Index"]}<h3/>
+        <h3>Location: {data["StationName"]}<h3/>
+        <br/>
+        <table>
+            <thead><tr>
+              <th>Parameter</th>
+              <th>Value</th>
+            </tr></thead>
+            <tbody>
+            <tr><td>Temperature</td><td>{data['Temperature']}\t°C</td></tr>
+            <tr><td>Humidity</td><td>{data['Humidity']}\t%</td></tr>
+            <tr><td>Pressure</td><td>{data['Pressure']}\thPa</td></tr>
+            <tr><td>PM10</td><td>{data['PM10']}\tµg/m3</td></tr>
+            <tr><td>PM2.5</td><td>{data['PM25']}\tµg/m3</td></tr>
+            </tbody>
+        </table><br/>
+        '''
+    return formatted
 
 @app.route('/')
 def home():
@@ -89,7 +123,6 @@ def home():
     response = get_air_data()
     result_dict = {}
     base_data = response['data']
-
     city = base_data["city"]["name"]
     geo = base_data["city"]["geo"]
     for key in base_data['iaqi']:
@@ -144,14 +177,15 @@ def history():
     conn, cur = connect_to_db()
 
     limit = request.args.get('limit', default=1)
+    result_string = "<h1>Air Polution Checker History<h1/>"
     
-    cur.execute("SELECT * FROM weather ORDER BY measurement_id DESC LIMIT 2;")
+    cur.execute("SELECT * FROM weather ORDER BY measurement_id DESC LIMIT {};".format(int(limit)))
     # cur.execute("""SELECT table_name FROM information_schema.tables
     #    WHERE table_schema = 'public'""")
     result = cur.fetchall()
     cur.close()
     conn.close()
-    result_string = "Result of arg parsing = {} <br/><br/>".format(limit)
     for res in result:
+        res = format_history(res)
         result_string = result_string + "{} <br/> ".format(res)
     return result_string
